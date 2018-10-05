2b5402db9124909de783af07e84f65f05ae5a28c
Yen-Cheng Chou <3055688+StevenYCChou@users.noreply.github.com>
2018-10-05 18:48:19 +0000

    parameterize the tool

--- tools/pr_helper.py
+++ tools/pr_helper.py
@@ -3,15 +3,34 @@
 TODO(ycchou): DO NOT SUBMIT without a detailed description of pr_helper.
 """
 
+from __future__ import print_function
+
+import argparse
 import re
 import requests
 
-google_fluentd_version = "v1.5.35"
-gem_repo_name = "fluent-plugin-google-cloud"
-gem_repo_pr = "232"
+argp = argparse.ArgumentParser(description='Check whether a gem is included in a version of google_fluentd.')
+argp.add_argument(
+    '--google_fluentd_version',
+    required=True,
+    help='the tag of google_fluentd_version.')
+argp.add_argument(
+    '--gem_name',
+    required=True,
+    help='the name of the gem.')
+argp.add_argument(
+    '--gem_repo_pull_number',
+    required=True,
+    help='the pull request number for the github repo of the gem.')
+
+args = argp.parse_args()
+
+google_fluentd_version = args.google_fluentd_version
+gem_name = args.gem_name
+gem_repo_pr = args.gem_repo_pull_number
 
 print("google_fluentd_version: {}".format(google_fluentd_version))
-print("gem_repo_name: {}".format(gem_repo_name))
+print("gem_name: {}".format(gem_name))
 print("gem_repo_pr: {}".format(gem_repo_pr))
 
 raw_plugin_gems_url = "https://raw.githubusercontent.com/GoogleCloudPlatform/google-fluentd/{}/plugin_gems.rb"
@@ -19,7 +38,7 @@ gem_pr_url = "https://api.github.com/repos/GoogleCloudPlatform/{}/pulls/{}"
 gem_compare_url = "https://api.github.com/repos/GoogleCloudPlatform/{}/compare/{}...v{}"
 download_regex_pattern = "download \"{}\", \"(.*)\""
 
-pattern = re.compile(download_regex_pattern.format(gem_repo_name))
+pattern = re.compile(download_regex_pattern.format(gem_name))
 gems = requests.get(raw_plugin_gems_url.format(google_fluentd_version), stream=True)
 for l in gems.iter_lines():
   m = pattern.match(l)
@@ -27,10 +46,10 @@ for l in gems.iter_lines():
     gem_version = m.group(1)
     print("gem version for google_fluentd {}: {}".format(google_fluentd_version, gem_version))
 
-commit_sha = requests.get(gem_pr_url.format(gem_repo_name, gem_repo_pr)).json()['merge_commit_sha']
-print("commit_sha for repo {} PR#{}: {}".format(gem_repo_name, gem_repo_pr, commit_sha))
+commit_sha = requests.get(gem_pr_url.format(gem_name, gem_repo_pr)).json()['merge_commit_sha']
+print("commit_sha for repo {} PR#{}: {}".format(gem_name, gem_repo_pr, commit_sha))
 
-compare_status = requests.get(gem_compare_url.format(gem_repo_name, commit_sha, gem_version)).json()['status']
+compare_status = requests.get(gem_compare_url.format(gem_name, commit_sha, gem_version)).json()['status']
 
 if compare_status == 'identical' or compare_status == 'ahead':
   print("this package includes your PR!")
