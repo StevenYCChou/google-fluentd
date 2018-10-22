f20615d7ea1be7a5a814128da7ff7bd7d32fdb06
Yen-Cheng Chou <ycchou@google.com>
2018-10-22 19:45:14 +0000

    refactor

--- tools/pr_helper.py
+++ tools/pr_helper.py
@@ -56,35 +56,25 @@ pr_url = "https://api.github.com/repos/" + org + "/{}/pulls/{}"
 compare_url = "https://api.github.com/repos/" + org + "/{}/compare/{}...{}"
 download_regex_pattern = "download \"{}\", \"(.*)\""
 
-if release_repo == pr_repo:
-  commit_sha = requests.get(pr_url.format(pr_repo, pr_number)).json()['merge_commit_sha']
-  print("commit_sha for repo {} PR#{}: {}".format(pr_repo, pr_number, commit_sha))
-  compare_status = requests.get(compare_url.format(pr_repo, commit_sha, release_version)).json()['status']
+if release_repo != pr_repo:
+  plugin_gems_rb = requests.get(raw_plugin_gems_url.format(release_repo, release_version), stream=True)
+  pattern = re.compile(download_regex_pattern.format(pr_repo))
 
-  if compare_status == 'identical' or compare_status == 'ahead':
-    print("this package includes your PR!")
-  else:
-    print("this package does not have your PR!")
-  exit(0)
-
-plugin_gems_rb = requests.get(raw_plugin_gems_url.format(release_repo, release_version), stream=True)
-
-pattern = re.compile(download_regex_pattern.format(pr_repo))
-
-for l in plugin_gems_rb.iter_lines():
-  m = pattern.match(l)
-  if m:
-    gem_version = m.group(1)
-    print("{} version {} contains upstream {}'s version {}".format(release_repo, release_version, pr_repo, gem_version))
-
-upstream_version = "v{}".format(gem_version)
+  for l in plugin_gems_rb.iter_lines():
+    m = pattern.match(l)
+    if m:
+      gem_version = m.group(1)
+      print("{} version {} contains upstream {}'s version {}".format(release_repo, release_version, pr_repo, gem_version))
+  target_version = "v{}".format(gem_version)
+else:
+  target_version = release_version
 
 commit_sha = requests.get(pr_url.format(pr_repo, pr_number)).json()['merge_commit_sha']
 print("commit_sha for repo {} PR#{}: {}".format(pr_repo, pr_number, commit_sha))
-
-compare_status = requests.get(compare_url.format(pr_repo, commit_sha, upstream_version)).json()['status']
+compare_status = requests.get(compare_url.format(pr_repo, commit_sha, target_version)).json()['status']
 
 if compare_status == 'identical' or compare_status == 'ahead':
   print("this package includes your PR!")
 else:
   print("this package does not have your PR!")
+
